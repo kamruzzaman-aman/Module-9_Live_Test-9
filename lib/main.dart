496a908adaeea5c5a1b258a542e526017c74eecb
KAMRUZZAMAN <kamrulcse1@gmail.com>
2023-10-06 23:29:51 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> sizes = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+  int selectedIndex = 0;
+
+  buttonColor(int index) {
+    selectedIndex = index;
+    setState(() {});
+  }
+
+  showSnackBar(int index) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected Size: ${sizes[index]}'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Size Selector'),
+      ),
+      body: Center(
+        child: Wrap(
+          spacing: 8,
+          children: [
+            for (int i = 0; i < sizes.length; i++)
+              ElevatedButton(
+                onPressed: () {
+                  buttonColor(i);
+                  showSnackBar(i);
+                },
+                child: Text(
+                  sizes[i],
+                  style: TextStyle(
+                    color: selectedIndex == i ? Colors.white : Colors.black,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: selectedIndex == i ? Colors.orange : Colors.grey,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
